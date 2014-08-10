4e07ef31d6736dc7ef8a453719ccb1ecd9afe2ca
zhangxv <925519307@qq.com>
2014-08-10 12:50:41 +0800

    TDD 22 Hooking Up Domain Model

--- finances/src/com/raingxm/finances/StockMarketTableModel.java
+++ finances/src/com/raingxm/finances/StockMarketTableModel.java
@@ -2,10 +2,22 @@ package com.raingxm.finances;
 
 import javax.swing.table.AbstractTableModel;
 
-public class StockMarketTableModel extends AbstractTableModel{
+public class StockMarketTableModel extends AbstractTableModel {
 	private static final long serialVersionUID = 1L;
-	private static final String[] COLUMN_TITLES = {"Year", "Starting Balance", "Starting Principal",
-								"Withdrawals", "Appreciation", "Deposits", "Ending Balance"};
+	private static final String[] COLUMN_TITLES = { "Year", "Starting Balance","Starting Principal", 
+					"Withdrawals", "Appreciation", "Deposits", "Ending Balance" };
+	
+	private int year;
+	private Dollars startingBalance;
+	private Dollars startingPrincipal;
+	private StockMarketYear marketYear;
+
+	public StockMarketTableModel(int year, Dollars startingBalance, Dollars startingPrincipal, InterestRate interestRate, TaxRate capitalGainsTaxRate) {
+		this.year = year;
+		this.marketYear = new StockMarketYear(startingBalance, startingPrincipal, interestRate, capitalGainsTaxRate);
+		this.startingBalance = startingBalance;
+		this.startingPrincipal = startingPrincipal;
+	}
 
 	@Override
 	public int getColumnCount() {
@@ -24,7 +36,13 @@ public class StockMarketTableModel extends AbstractTableModel{
 
 	@Override
 	public Object getValueAt(int rowIndex, int columnIndex) {
-		return columnIndex;
+		switch (columnIndex) {
+			case 0: return year;
+			case 1: return marketYear.startingBalance();
+			case 2: return marketYear.startingPrincipal(); 
+			case 3: return marketYear.totalWithdrawn();
+			default: return "";
+		}
 	}
 
 }
--- finances/src/com/raingxm/finances/_StockMarketTableModelTest.java
+++ finances/src/com/raingxm/finances/_StockMarketTableModelTest.java
@@ -7,11 +7,14 @@ import org.junit.Test;
 
 public class _StockMarketTableModelTest {
 
+	private static final int YEAR = 2010;
+	private static final Dollars STARTING_BALANCE = new Dollars(10000);
+	private static final Dollars STARTING_PRINCIPAL = new Dollars(7000);
 	private StockMarketTableModel model;
 
 	@Before
 	public void setup() {
-		model = new StockMarketTableModel();
+		model = new StockMarketTableModel(YEAR, STARTING_BALANCE, STARTING_PRINCIPAL, new InterestRate(10), new TaxRate(25));
 	}
 
 	@Test
@@ -22,9 +25,16 @@ public class _StockMarketTableModelTest {
 		assertEquals("Starting Principal", model.getColumnName(2));
 	}
 
+	private static final String[] COLUMN_TITLES = { "Year", "Starting Balance","Starting Principal", 
+		"Withdrawals", "Appreciation", "Deposits", "Ending Balance" };
+	
 	@Test
 	public void firstRow() {
-
+		assertEquals(YEAR, model.getValueAt(0, 0));
+		assertEquals(STARTING_BALANCE, model.getValueAt(0, 1));
+		assertEquals(STARTING_PRINCIPAL, model.getValueAt(0, 2));
+		assertEquals(new Dollars(0), model.getValueAt(0, 3));
+		assertEquals(new Dollars(1000), model.getValueAt(0, 4));
 	}
 
 }
